7257a3e4916c0bd7c3493d4981d8a392226f25a6
PluckyJokerhead <pluckyjokerhead@gmail.com>
2018-05-27 21:23:07 +0100

    program progress and logo
    application now loads classicshell.exe and has buttons for all of the other currently intended features

--- main application/WindowsFormsApp2/MainGUI.Designer.cs
+++ main application/WindowsFormsApp2/MainGUI.Designer.cs
@@ -29,33 +29,152 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(12, 81);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 73);
+            this.button1.Size = new System.Drawing.Size(145, 76);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Install Classic Shell";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 76);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Apply Classic Shell Settings";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 76);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Disable Telemetry";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(335, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 76);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Disable Cortana";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(174, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 76);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Disable Forced Updates";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(336, 81);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(144, 76);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Install Soundswitch";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(497, 177);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(144, 76);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Apply Windows 7 Sounds";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(497, 81);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(144, 76);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Change Default Programs";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(262, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Windows Zen";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.65F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(497, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Created by Chris Seadon";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MainGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(667, 424);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.MinimumSize = new System.Drawing.Size(340, 260);
+            this.Name = "MainGUI";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- main application/WindowsFormsApp2/MainGUI.cs
+++ main application/WindowsFormsApp2/MainGUI.cs
@@ -13,25 +13,59 @@ namespace WindowsFormsApp2
         // Downloads classic shell and calls install function when complete
         public void DownloadClassicShell()
         {
+            Application.UseWaitCursor = true;
             var client = new WebClient();
             var uri = new Uri("https://sourceforge.net/projects/classicshell/files/latest/download");
-            client.DownloadFileCompleted += (sender, e) => install();
+            client.DownloadFileCompleted += (sender, e) => installclassicshell();
             client.DownloadFileAsync(uri, "classicshell.exe");
         }
-        // trying to figure out how a add a couple seconds wait as a safety measure
-        private static System.Timers.Timer aTimer;
-        public void install()
-        {
-            Console.WriteLine("yeah");
-            aTimer = new System.Timers.Timer();
 
-            Console.WriteLine("yeahh");
+        public void installclassicshell()
+        {
+            Console.WriteLine("Downloaded classicshell.exe");
+            //wait 5 seconds
+            DateTime Tthen = DateTime.Now;
+            do
+            {
+                Application.DoEvents();
+            } while (Tthen.AddSeconds(5) > DateTime.Now);
+            //open the file
+            Console.WriteLine("Opening it");
+            Application.UseWaitCursor = false;
+            System.Diagnostics.Process.Start("classicshell.exe");
+            warning1 dialog = new warning1();
+            dialog.Show();
         }
+
         //when button 1 is clicked downloadclassicshell function is called
         private void button1_Click(object sender, EventArgs e)
         {
             DownloadClassicShell();
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
